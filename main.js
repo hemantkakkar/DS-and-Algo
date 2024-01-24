9a5d83349845be6461eda67f552b69c4547cfc8a
Hemant Kakkar <hemantkakkar77@gmail.com>
2024-01-24 12:30:29 +0530

    Adding Linear search algo implemantation

--- main.js
+++ main.js
@@ -21,3 +21,44 @@ function fibonacci (n) {
 }
 
 console.log(fibonacci(7))
+
+// Prime Number
+function isPrime(n){
+  if(n<2){
+    return false
+  }
+  for(i=2; i<Math.sqrt(n); i++){
+    if(n%i === 0){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(isPrime(5))
+
+// Power of 2
+function isPowerOfTwo(n) {
+  if(n<1){
+    return false
+  }
+  while(n>1){
+    if(n%2 !== 0){
+      return false
+    }
+    n = n/2
+  }
+  return true
+}
+
+// Linear search
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] === target) {
+      return i
+    }
+  }
+  return -1
+}
+
+console.log(linearSearch([1,3,5], 5))
